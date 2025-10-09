d2e83e724ae04149abd6c180ab69da7adad99133
quangloc1402 <tranquangloc2003@gmail.com>
2025-10-09 18:17:58 +0700

    call api delete student from class

--- src/service/courseStudentService.js
+++ src/service/courseStudentService.js
@@ -1,12 +1,21 @@
 import api from "../config/axios";
 
-
 export const getStudentsInCourse = async (courseInstanceId) => {
   try {
     const response = await api.get(`/CourseStudent/course-instance/${courseInstanceId}`);
     return response.data.data;
   } catch (error) {
     console.error("Get Course Students Failed:", error);
+    throw error;
+  }
+};
+
+export const removeStudentFromCourse = async (userId, courseInstanceId, courseStudentId) => {
+  try {
+    const response = await api.delete(`/CourseStudent/delete?userId=${userId}&courseInstanceId=${courseInstanceId}&courseStudentId=${courseStudentId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Remove Student Failed:", error.response?.data || error.message);
     throw error;
   }
 };
