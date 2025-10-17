f722481b9c920c9beabf000050d59fb610e003f0
quangloc1402 <tranquangloc2003@gmail.com>
2025-10-18 00:55:50 +0700

    manage criteria ui

--- src/layout/InstructorClassLayout.jsx
+++ src/layout/InstructorClassLayout.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
-import InstructorHeader from "../component/Header/IntructorHeader"; 
+import InstructorHeader from "../component/Header/IntructorHeader";
 import InstructorSideBar from "../component/SideBar/InstructorSideBar";
 
-function InstructorLayout() { 
+function InstructorLayout() {
   return (
     <div>
       <InstructorHeader />
@@ -10,13 +10,15 @@ function InstructorLayout() {
       <div
         className="main-content"
         style={{
-          marginLeft: "256px", 
+          marginLeft: "256px",
           width: "calc(100% - 256px)",
           minHeight: "100vh",
-          padding: "20px"
+
         }}
       >
-        <Outlet />
+        <div className="main-content p-8">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
