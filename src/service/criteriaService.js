d746617cbf5b6ffabd95572149d899797f8a502a
quangloc1402 <tranquangloc2003@gmail.com>
2025-10-18 02:49:41 +0700

    call api delete criteria

--- src/service/criteriaService.js
+++ src/service/criteriaService.js
@@ -1,15 +1,26 @@
 import api from "../config/axios";
 
 export const getCriteriaByRubricId = async (rubricId) => {
-  try {
-    const response = await api.get(`/Criteria/rubric/${rubricId}`);
-    return response.data.data;
-  } catch (error) {
-    console.error(`Error fetching criteria for rubric ${rubricId}:`, error);
-    throw error;
-  }
+    try {
+        const response = await api.get(`/Criteria/rubric/${rubricId}`);
+        return response.data.data;
+    } catch (error) {
+        console.error(`Error fetching criteria for rubric ${rubricId}:`, error);
+        throw error;
+    }
+};
+
+export const deleteCriterion = async (criterionId) => {
+    try {
+        const response = await api.delete(`/Criteria/${criterionId}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error deleting criterion ${criterionId}:`, error);
+        throw error;
+    }
 };
 
 export const criteriaService = {
-  getCriteriaByRubricId,
+    getCriteriaByRubricId,
+    deleteCriterion,
 };
