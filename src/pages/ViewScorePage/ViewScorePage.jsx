41a00bd4d11573ec0fc19c312bf683b52d4e849a
DuydoFE <manhlon18@gmail.com>
2025-11-04 02:05:40 +0700

    missing import useState

--- src/pages/ViewScorePage/ViewScorePage.jsx
+++ src/pages/ViewScorePage/ViewScorePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { reviewService } from "../../service/reviewService";
