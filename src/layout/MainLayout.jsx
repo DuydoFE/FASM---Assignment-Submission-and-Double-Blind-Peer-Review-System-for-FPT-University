79fb991a84a51945c93ea7660f0bfe0dc3c80458
DuydoFE <manhlon18@gmail.com>
2025-11-18 13:30:12 +0700

    Add GSAP animations and enhance UI components with new styles and effects

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,17 +1,29 @@
-// src/pages/Layout.jsx
 import Header from "../component/Header/Header";
-import Footer from "../component/Footer/Footer"; // Bạn có thể muốn tạo Footer theme tối
+import Footer from "../component/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import Aurora from "@/components/Aurora"; 
 
 function Layout() {
   return (
-    // Nền tối cho toàn bộ ứng dụng
-    <div className="bg-orange-50 text-zinc-800">
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="relative min-h-screen bg-black text-zinc-200">
+      <Aurora
+        className="absolute top-0 left-0 w-full h-full z-0"
+        
+        
+        colorStops={['#0055FF', '#FF8C00', '#20BF55']}
+
+        blend={0.5}
+        amplitude={1.0}
+        speed={0.2}
+      />
+      
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
