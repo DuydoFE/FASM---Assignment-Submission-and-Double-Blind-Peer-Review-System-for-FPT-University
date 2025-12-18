984e32eba46fdd6035eed8095dd764a75912d8f7
quangloc1402 <tranquangloc2003@gmail.com>
2025-12-19 01:53:17 +0700

    update

--- src/pages/InstructorManageAssignment/InstructorManageAssignment.jsx
+++ src/pages/InstructorManageAssignment/InstructorManageAssignment.jsx
@@ -85,6 +85,7 @@ const InstructorManageAssignment = () => {
         ).toLocaleDateString(),
         finalDeadline: new Date(assignment.finalDeadline).toLocaleDateString(),
         time: new Date(assignment.deadline).toLocaleTimeString(),
+        reviewTime: new Date(assignment.reviewDeadline).toLocaleTimeString(),
         submitted: assignment.reviewCount,
         total: assignment.submissionCount,
         courseCode: assignment.courseCode,
@@ -304,7 +305,7 @@ const InstructorManageAssignment = () => {
           },
         ]
       : []),
-    ...(assignment.status === "Draft" || assignment.status === "Upcoming"
+    ...(assignment.status === "Draft"
       ? [
           {
             label: (
@@ -326,7 +327,7 @@ const InstructorManageAssignment = () => {
       ),
       onClick: () => handleViewSubmissions(assignment),
     },
-    ...(assignment.status === "Draft" || assignment.status === "Upcoming"
+    ...(assignment.status === "Draft"
       ? [
           {
             type: "divider",
@@ -479,7 +480,7 @@ const InstructorManageAssignment = () => {
               >
                 {assignment.reviewDeadline}
               </div>
-              <div className="text-sm text-gray-500">{assignment.time}</div>
+              <div className="text-sm text-gray-500">{assignment.reviewTime}</div>
             </div>
             <div className="col-span-2 flex justify-center">
               <span
